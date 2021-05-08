e587ce1de7f09a391dfa467663ed51876875f768
Bhargav Ponnapalli <imbhargav5@gmail.com>
2021-05-08 14:40:41 +0530

    minor bug fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 // next.config.js
 
-module.exports = {
-    experimental: {
-      reactMode: 'concurrent'
-    }
-}
+const withTM = require('next-transpile-modules')(['rooks']);
+const withPlugins = require('next-compose-plugins');
+
+const nextConfig = {
+  experimental: {
+    reactMode: 'concurrent',
+  },
+};
+
+module.exports = withPlugins([withTM], nextConfig);
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,17 @@
 import { RelayEnvironmentProvider } from 'react-relay/hooks';
-import {RelayEnvironment} from '@/config/relay/RelayEnvironment'
+import { RelayEnvironment } from '@/config/relay/RelayEnvironment';
+import { useDidMount, useKey, useMouse, useWillUnmount } from 'rooks';
 
 export default function App({ Component, pageProps }) {
+  useDidMount(() => {
+    console.log('hello world');
+  });
+
+  const val = useMouse();
 
   return (
     <RelayEnvironmentProvider environment={RelayEnvironment}>
-        <Component {...pageProps} />
+      <Component {...pageProps} />
     </RelayEnvironmentProvider>
-  )
+  );
 }
